51c7953125553220da38be34bf4a0bdcc4e882a9
Adam Phillips <adam2000phillips@gmail.com>
2021-06-21 23:29:27 +0100

    Third test, instruct a plane not to land into a full airport

--- Airport_Makers/spec/plane_spec.rb
+++ Airport_Makers/spec/plane_spec.rb
@@ -4,9 +4,21 @@ describe Plane do
   before(:each) do
     @plane = Plane.new
   end
-  describe '#land' do
+  let(:air_traffic_control) {double :air_traffic_control}
+  describe '#land when airport is not full' do
+    before do
+      allow(@plane.air_traffic_control).to receive(:full?).and_return(false)
+    end
     it "lands at the designated airport" do
       expect(@plane.land).to eq "Plane landed sucessfully!"
+    end
+  end
+  describe '#land when airport is full' do
+    before do
+      allow(@plane.air_traffic_control).to receive(:full?).and_return(true)
+    end
+    it "cannot land when the airport is full" do
+      expect(@plane.land).to raise_error 'Airport is full, plane cannot land!'
     end
   end
   describe '#take_off' do
