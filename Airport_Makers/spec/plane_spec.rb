f6a53d17b71d20f77d516d0e118d45fbb92221fe
Adam Phillips <adam2000phillips@gmail.com>
2021-06-21 20:44:11 +0100

    First test, instruct a plane to land at an airport

--- Airport_Makers/spec/plane_spec.rb
+++ Airport_Makers/spec/plane_spec.rb
@@ -1,5 +1,12 @@
 require 'plane'
 
 describe Plane do
-
+  before(:each) do
+    @plane = Plane.new
+  end
+  describe '#land' do
+    it "lands at the designated airport" do
+      expect(@plane.land).to eq "Plane landed sucessfully!"
+    end
+  end
 end 
