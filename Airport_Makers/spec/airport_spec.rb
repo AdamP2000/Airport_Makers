00c0bd5881d6019a3106b989f2cc0c86b6147995
Adam Phillips <adam2000phillips@gmail.com>
2021-06-24 10:29:41 +0100

    Fourth test, each airport has a default capacity to ensure safety.

--- Airport_Makers/spec/airport_spec.rb
+++ Airport_Makers/spec/airport_spec.rb
@@ -1,5 +1,12 @@
 require 'airport'
 
 describe Airport do
-  
+  before(:each) do
+    @airport = Airport.new
+  end
+  describe '#capacity' do
+    it 'has a default capacity' do
+      expect(@airport.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+  end
 end
